dfabe1b64099b91b8701a04784bfcaacc44f9af3
Marc Antoine Griffiths-Lorange <marc.xd866@gmail.com>
2024-08-07 18:56:23 -0400

    Almost ready for V1.0

--- include/button.h
+++ include/button.h
@@ -1,8 +1,11 @@
 #ifndef ESP32_BUTTON_H
 #define ESP32_BUTTON_H
 
-#include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
 
 #ifndef CONFIG_ESP32_BUTTON_LONG_PRESS_DURATION_MS
 #define CONFIG_ESP32_BUTTON_LONG_PRESS_DURATION_MS (2000)
@@ -21,22 +24,26 @@
 #endif
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-#define PIN_BIT(x) (1ULL<<x)
+#define PIN_BIT(x) (1ULL << x)
 
 #define BUTTON_DOWN (1)
 #define BUTTON_UP (2)
 #define BUTTON_HELD (3)
 
-typedef struct {
-  uint8_t pin;
+  typedef struct
+  {
+    uint8_t pin;
     uint8_t event;
-} button_event_t;
+  } button_event_t;
 
-QueueHandle_t button_init(unsigned long long pin_select);
-QueueHandle_t pulled_button_init(unsigned long long pin_select, gpio_pull_mode_t pull_mode);
+  void button_sleep_in();
+  void button_sleep_out();
+  QueueHandle_t button_init(unsigned long long pin_select);
+  QueueHandle_t pulled_button_init(unsigned long long pin_select, gpio_pull_mode_t pull_mode);
 
 #ifdef __cplusplus
 }
